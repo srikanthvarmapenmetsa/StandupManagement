d03542fbdea356c7d0d58f2a7a8f9a9dbeb7fb4c
sriaknthvarmapenmetsa <srikanth.penmetsa@gmail.com>
2016-09-08 08:47:34 +0530

    Added Solution

---
+++ StandupManagement.UI/app/Services/resource.service.ts
@@ -0,0 +1,19 @@
+﻿import { Injectable } from 'angular2/core';
+import {IResource} from '../Interfaces/resource'
+import {Http, Response} from 'angular2/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/Rx';
+
+@Injectable()
+export class ResourceService {
+    private _resourceUrl = "http://localhost/StandupManagement.Services/ResourceService.svc/GetResources";
+    constructor(private _http: Http) {
+    }
+
+    getResources(): Observable<IResource[]> {
+        if (this._http != null) {
+            return this._http.get(this._resourceUrl)
+                .map((res: Response) => <IResource[]>JSON.parse(res.json()));
+        }
+    }
+}
---
+++ StandupManagement.UI/app/Standup/standups.component.ts
@@ -0,0 +1,28 @@
+﻿import { Component, OnInit} from 'angular2/core';
+import {ResourceService} from '../Services/resource.service'
+import {IResource} from '../Interfaces/resource'
+import {HTTP_PROVIDERS} from 'angular2/http';
+import {Observable} from 'rxjs/Observable';
+
+@Component({
+    selector: 'pm-standups',
+    templateUrl: 'app/Standup/standups.component.html',
+    providers: [ResourceService, HTTP_PROVIDERS]
+})
+
+export class StandupsComponent implements OnInit {
+    selectedResource: string = "test";
+    header: string = "Team Report";
+    availableResources: IResource[];
+    constructor(private _resourceService: ResourceService) {
+    }
+    ngOnInit(): void {
+        this._resourceService.getResources().subscribe(p=>this.availableResources=p);
+    }
+    //ngDoCheck() {
+    //    if (this.selectedResource != "5") {
+    //        alert(this.selectedResource);
+    //    }
+    //}
+
+}
